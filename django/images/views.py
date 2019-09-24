a9d77fa3888642618d295d837f2c982c474f9461
v3ct0r22 <rekinvector@gmail.com>
2019-09-24 10:55:15 +0000

    Using redis to storing image item views

--- django/images/views.py
+++ django/images/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.shortcuts import get_object_or_404
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.conf import settings
 
 from .forms import ImageCreateForm
 from .models import Image
 
 from common.decorators import ajax_required
 from actions.utils import create_action
+
+import redis
+
+# Connect to redis
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                      port=settings.REDIS_PORT,
+                      db=settings.REDIS_DB)
 
 
 @login_required
@@ -70,10 +77,13 @@ def image_list(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    # Use redis to increment total image views by 1
+    total_views = r.incr('image:{}:views'.format(image.id))
     return render(request,
                   'images/image/detail.html',
                   {'section': 'images',
-                   'image': image})
+                   'image': image,
+                   'total_views': total_views})
 
 
 @ajax_required
